c92cb5b4a5bf5920f930f04e5988f19251538f42
Toshiaki Maki <tmaki@vmware.com>
2023-05-17 06:53:32 +0900

    Update

--- src/main/java/com/example/TweetViewerApplication.java
+++ src/main/java/com/example/TweetViewerApplication.java
@@ -15,14 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class TweetViewerApplication {
 	private final List<Tweet> tweets = new CopyOnWriteArrayList<>();
 
-	@GetMapping
+	@GetMapping(path = "/")
 	public List<Tweet> viewTweets() {
 		return this.tweets;
 	}
 
 	@Bean
 	public Consumer<Tweet> tweetCollector() {
-		return this.tweets::add;
+		return tweet -> this.tweets.add(tweet);
 	}
 
 	public static void main(String[] args) {
